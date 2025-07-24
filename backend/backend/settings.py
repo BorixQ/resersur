125f7b16d718cdac0ef628376921fb13fd523dc5
BorixQ <borixquiroz@gmail.com>
2025-07-24 11:48:01 -0500

    Configurar Docker y base de datos MySQL, asegurar conexión con Django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'taller_mecanico',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
